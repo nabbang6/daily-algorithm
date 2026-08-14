b87bf76b0c7ee2d3e27047bcf9584df98375803f
nabbang6 <hawwah03@gachon.ac.kr>
2026-08-14 14:45:24 +0900

    feat: 연속된 수의 합 풀이 추가

---
+++ programmers/Lv.0/연속된 수의 합/solution.js
@@ -0,0 +1,12 @@
+function solution(num, total) {
+    var answer = [];
+    let start = (total / num) - ((num - 1) / 2);
+
+    for (let i = 0; i < num; i++) {
+        answer.push(start + i);
+    }
+
+    return answer;
+}
+
+console.log(solution(3, 12));
